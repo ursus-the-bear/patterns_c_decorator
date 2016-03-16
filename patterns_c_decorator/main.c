de9c822876d9380f63afeba4358bc3a05c4f87e2
ursus-the-bear <develop@ursus.cc>
2016-03-16 19:41:13 +0100

    Marcel help

--- patterns_c_decorator/main.c
+++ patterns_c_decorator/main.c
@@ -1,59 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct _toppingDecorator toppingDecorator_t;
+typedef struct _pizza pizza_t;
+typedef double (* getCost)(struct _pizza * self);
 
 typedef struct _pizza {
-    double (* getCost) (toppingDecorator_t * self);
+    getCost getCostFunc;
 } pizza_t;
 
+typedef struct _plainPizza {
+    pizza_t base;
+} plainPizza_t;
+
 typedef struct _toppingDecorator {
-    
-    pizza_t tempPizza;
-    pizza_t * delegate;
-    
+    pizza_t base;
+    pizza_t * decorate;
 } toppingDecorator_t;
 
-typedef struct _topping {
-    
-    toppingDecorator_t methods;
-    pizza_t * delegate;
-    
-} topping_t;
-
-typedef struct _order {
-    
-    pizza_t pizza;
-    
-    // something else? list of toppings?
-    
-} order_t;
-
 // these are the pizzas
-double plainPizzaCost (toppingDecorator_t * self) {
+double plainPizzaCost (plainPizza_t self) {
     return 5;
 }
 
 // these are the toppings
 double mozzarellaCost (toppingDecorator_t * self) {
-    return 3.0;
+    return self->decorate->getCostFunc(self->decorate) + 3;
 }
 double tomatoCost (toppingDecorator_t * self) {
-    return 1;
+    return self->decorate->getCostFunc(self->decorate) + 2;
+}
+double peparoniCost (toppingDecorator_t * self) {
+    return self->decorate->getCostFunc(self->decorate) + 1;
 }
 
 int main(int argc, const char * argv[]) {
     
-    order_t myOrder;
-    myOrder.pizza.getCost = &plainPizzaCost;
-    printf ("My pizza costs %f\n", myOrder.pizza.getCost ((toppingDecorator_t *) &myOrder.pizza));
+    plainPizza_t plainPizza;
+    plainPizza.base.getCostFunc = (getCost) plainPizzaCost;
     
-    toppingDecorator_t toppings;
-    toppings.delegate = (pizza_t *) &myOrder;
+    toppingDecorator_t mozzarella;
+    mozzarella.base.getCostFunc = (getCost) mozzarellaCost;
+    mozzarella.decorate = (pizza_t *) &plainPizza;
     
-    topping_t mozzarella;
-    mozzarella.delegate = (pizza_t *) &myOrder;
-    mozzarella.methods.delegate = (pizza_t *)&mozzarellaCost;
-    printf ("My pizza costs %f\n", mozzarella.delegate->getCost ((toppingDecorator_t *) &mozzarella));
+    toppingDecorator_t tomato;
+    tomato.base.getCostFunc = (getCost) tomatoCost;
+    tomato.decorate = (pizza_t *) &mozzarella;
+    
+    toppingDecorator_t salami;
+    salami.base.getCostFunc = (getCost) peparoniCost;
+    salami.decorate = (pizza_t *) &tomato;
     
     
+    printf ("A plain pizza %f\n", plainPizza.base.getCostFunc((pizza_t *) &plainPizza));
+    printf ("A salami pizza (incl. tomato and mozzarella) costs %f\n", salami.base.getCostFunc((pizza_t *) &salami));
 }
